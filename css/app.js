9f5c4e1a8e0f50f88bb93e1d12e16bcf8d4af8c3
r4Y-s1Lv3r <ottoemil.heiskanen@gmail.com>
2023-07-10 18:21:54 +0300

    Initial commit

---
+++ css/app.js
@@ -0,0 +1,138 @@
+/*addEventListener('click', createBox);
+
+function createBox(event) {
+    var mark = document.createElement('div');
+    mark.className = 'mark';
+    mark.style.left = event.pageX + 'px';
+    mark.style.top = event.pageY + 'px';
+    document.body.appendChild(mark);
+}*/
+
+window.onload = function() {
+    if ( document.URL.includes("index.html") ) {
+        var somenum = 0
+        localStorage.setItem("storageName", somenum);
+    } else {
+        var distance = 0;
+        distance = localStorage.getItem("storageName");
+        let txt = document.getElementById("text")
+        txt.textContent = "Your score: " + Math.round(parseInt(distance));
+    }
+};
+
+/*window.onload = function() {
+    if (localStorage.getItem("storageName") == null) {
+        var somenum = 0
+        localStorage.setItem("storageName", somenum);
+    }
+    if ( document.URL.includes("index.html") ) {
+        var somenum = 0
+        localStorage.setItem("storageName", somenum);
+    } else {
+        var distance = localStorage.getItem("storageName");
+        let txt = document.getElementById("text")
+        txt.textContent = "Your score: " + Math.round(parseInt(distance));
+    }
+};*/
+
+/*window.onload = function() {
+    try{        
+        if ( document.URL.includes("index.html") ) {
+            var somenum = 0
+            localStorage.setItem("storageName", somenum);
+        } else {
+            var distance = localStorage.getItem("storageName");
+            let txt = document.getElementById("text")
+            txt.textContent = "Your score: " + Math.round(parseInt(distance));
+        }
+    }catch (e){
+        var somenum = 0
+        localStorage.setItem("storageName", somenum);
+    }
+};*/
+
+/*window.onload = function() {
+
+    if ( document.URL.includes("index.html") || localStorage.getItem("storageName") == NaN || localStorage.getItem("storageName") == null) {
+        var somenum = 0
+        localStorage.setItem("storageName", somenum);
+    } else {
+        var distance = localStorage.getItem("storageName");
+        let txt = document.getElementById("text")
+        txt.textContent = "Your score: " + Math.round(parseInt(distance));
+    }
+    
+};*/
+
+var box = document.getElementById("mark");
+    addEventListener('click', function(event) {
+    box.style.visibility="visible";
+    box.style.left = event.pageX + 'px';
+    box.style.top = event.pageY + 'px';
+    let sub = document.getElementById("btn");
+    sub.style.visibility = "visible";
+    let bt = document.getElementById("next");
+    bt.style.width = 9 + 'vw';
+}, { once: true });
+
+let button = document.getElementById("btn");
+
+button.addEventListener("click", () => {
+    let guess = document.getElementById("mark");
+    let real = document.getElementById("fs");
+
+    const pos1 = guess.getBoundingClientRect();
+    const pos2 = real.getBoundingClientRect();
+
+    var xa = pos2.left - pos1.left
+    var ya = pos2.top - pos1.top
+    if (xa < 0) {
+        var xaw = xa/-1
+    } else {
+        var xaw = xa
+    }
+    if (ya < 0) {
+        var yaw = ya/-1
+    } else {
+        var yaw = ya
+    }
+    var dis = xaw*2 + yaw*2
+    /*if (dis < 0) {
+        var dis2 = dis/-1;
+    } else {
+        var dis2 = dis;
+    }*/
+    var distance = Math.sqrt(dis);
+
+    if (Math.round(distance) > 25) {
+        distance = 0;
+    } else if (Math.round(distance) <= 3) {
+        distance = 25
+    } else {
+        distance = 25 - distance
+    }
+
+    distance = Math.round(parseInt(localStorage.getItem("storageName")) + distance)
+
+    let txt = document.getElementById("text");
+    txt.textContent = "Your score: " + distance;
+    
+    localStorage.setItem("storageName", parseInt(distance));
+
+    real.style.visibility="visible";
+    let sub = document.getElementById("btn");
+    let nxt = document.getElementById("next");
+
+    sub.style.visibility = "hidden";
+    nxt.style.visibility = "visible";
+});
+
+/*I guess. here are some building blocks:
+to get an element based on its id, you use document.getElementById(id_goes_here)
+let myElement = document.getElementById("the-id-to-select");
+To run code when a certain element is clicked, you use the addEventListener function:
+element.addEventListener("click", () => {
+  // code here will run when the element is clicked
+});
+to get the position of elements on a page, you can use the getClientBoundingRect method:
+const myRect = element.getClientBoundingRect();*/
